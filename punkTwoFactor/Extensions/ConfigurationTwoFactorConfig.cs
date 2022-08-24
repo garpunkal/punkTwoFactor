922fcd81807ca9ee69d9285ff1698eaebd63f331
Gareth Wright <garpunkal@gmail.com>
2022-08-24 10:19:15 +0100

    extension for configuration

---
+++ punkTwoFactor/Extensions/ConfigurationTwoFactorConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using punkTwoFactor.Models;
+
+namespace punkTwoFactor.Extensions
+{
+    public static class ConfigurationTwoFactorConfig
+    {
+        public static TwoFactorConfig ConfigureTwoFactorConfig(
+            this IServiceCollection services, IConfiguration config, string configName = "punkTwoFactor")
+        {
+            services.Configure<TwoFactorConfig>(config.GetSection(configName));
+            TwoFactorConfig twoFactorConfig = new();
+            config.GetSection(configName).Bind(twoFactorConfig);
+            return twoFactorConfig;
+        }
+    }
+}
